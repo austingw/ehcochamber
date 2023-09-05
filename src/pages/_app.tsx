2980f48aeda79ba396c0addae2ba096ee73117f6
austingw <senortinman@gmail.com>
2023-09-05 10:57:40 -0400

    Remove global css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,6 @@ import { type AppType } from "next/app";
 import { MantineProvider } from "@mantine/core";
 import { api } from "@/utils/api";
 
-import "@/styles/globals.css";
-
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
