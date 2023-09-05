5a90006b1aed40d9ffe9f42a9097c17482c20dfc
austingw <senortinman@gmail.com>
2023-09-04 20:49:51 -0400

    Add basic Mantine implementation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { type AppType } from "next/app";
-
+import { MantineProvider } from "@mantine/core";
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
