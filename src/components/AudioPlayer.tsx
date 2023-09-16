fa2434d7601acbbe1eddf1279b26c09f0d4ebca2
austingw <senortinman@gmail.com>
2023-09-15 20:36:36 -0400

    Consolidate waveform/audioplayer as it works better lol

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,29 +1,23 @@
 import formatTime from "@/utils/formatTime";
 import { Button } from "@mantine/core";
 import { useEffect, useRef, useState } from "react";
-import Waveform from "./Waveform";
-import { api } from "@/utils/api";
+
+import WaveSurfer from "wavesurfer.js";
+import React from "react";
+
+const style = {
+  width: "100%",
+  height: "100%",
+  backgroundColor: "#333",
+};
 
 const AudioPlayer = () => {
   const url =
     "https://xntslrrernpkzvgsuipl.supabase.co/storage/v1/object/sign/Audio/oh-my-god-bro-oh-hell-nah-man.mp3?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJBdWRpby9vaC1teS1nb2QtYnJvLW9oLWhlbGwtbmFoLW1hbi5tcDMiLCJpYXQiOjE2OTQ4MjI0NTQsImV4cCI6MTcyNjM1ODQ1NH0.J7c1JpcJALKKMO-o7AsDlfeMX5gQlco1iensjtjIJ0E&t=2023-09-16T00%3A00%3A54.942Z";
 
-  const audioRef = useRef<HTMLAudioElement>(null);
-
   const [isPlaying, setIsPlaying] = useState(false);
   const [duration, setDuration] = useState(0);
-
-  useEffect(() => {
-    if (!audioRef.current) return;
-
-    const wavesurfer = async () => {
-      if (isPlaying) {
-        await audioRef?.current?.play();
-      } else audioRef?.current?.pause();
-    };
-
-    wavesurfer().catch((err) => console.error(err));
-  }, [isPlaying, audioRef]);
+  const waveformRef = useRef<string | HTMLAudioElement>("");
 
   const togglePlay = () => {
     setIsPlaying((prev) => !prev);
@@ -32,18 +26,52 @@ const AudioPlayer = () => {
     setIsPlaying(false);
   };
   const handleAudioLoad = () => {
-    if (audioRef.current) setDuration(audioRef.current.duration);
+    if (waveformRef.current) setDuration(waveformRef.current.duration);
   };
+
+  useEffect(() => {
+    const createWaveform = async () => {
+      const wavesurfer = WaveSurfer.create({
+        container: waveformRef.current,
+        mediaControls: false,
+        waveColor: "#eee",
+        progressColor: "#0178FF",
+        cursorColor: "OrangeRed",
+        barWidth: 3,
+        barRadius: 3,
+        height: 150,
+        normalize: true,
+      });
+
+      await wavesurfer.load(url);
+
+      return wavesurfer;
+    };
+
+    createWaveform()
+      .then((wavesurfer) => {
+        if (isPlaying) {
+          wavesurfer.play().catch((err) => console.error(err));
+        } else {
+          wavesurfer.pause();
+        }
+
+        wavesurfer.getCurrentTime();
+      })
+      .catch((err) => console.error(err));
+
+    const current = waveformRef?.current;
+
+    return () => {
+      if (current?.innerHTML) {
+        current.innerHTML = "";
+      }
+    };
+  }, [url, isPlaying]);
 
   return (
     <>
-      <audio
-        src={url}
-        ref={audioRef}
-        onEnded={handleAudioEnd}
-        onLoadedData={handleAudioLoad}
-      />
-      <Waveform url={url} isPlaying={isPlaying} audioRef={audioRef} />
+      <div style={style} id="waveform" ref={waveformRef} />
       {formatTime(duration)}
       <Button onClick={togglePlay}>{isPlaying ? "Pause" : "Play"}</Button>
     </>
