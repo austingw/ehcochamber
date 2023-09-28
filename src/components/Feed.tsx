8e5357a56e76cf0b3ccadc475939d3e5e97c08a8
austingw <senortinman@gmail.com>
2023-09-28 18:38:00 -0400

    Add responsive size to modal

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -2,17 +2,19 @@ import { Flex, Modal } from "@mantine/core";
 import PostCard from "./PostCard";
 import { useState } from "react";
 import Post from "./Post";
-import { useDisclosure } from "@mantine/hooks";
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 
 const Feed = () => {
   const [selectedPost, setSelectedPost] = useState<string | null>(null);
   const [opened, { open, close }] = useDisclosure(false);
+  const isMobile = useMediaQuery("(max-width: 768px)");
 
   const testPosts = [
     {
       id: 1,
       url: "https://xntslrrernpkzvgsuipl.supabase.co/storage/v1/object/sign/Audio/Two%20Pillars(7).wav?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJBdWRpby9Ud28gUGlsbGFycyg3KS53YXYiLCJpYXQiOjE2OTQ5ODU3OTUsImV4cCI6MTcyNjUyMTc5NX0.2jcTsYUzbSXxLKnrxpzgpB0dYxqhVymprddFt80e39g&t=2023-09-17T21%3A23%3A15.979Z",
       art: "",
+      title: "Two Pillars",
     },
     {
       id: 2,
@@ -71,10 +73,12 @@ const Feed = () => {
       <Modal
         opened={opened}
         onClose={close}
-        withCloseButton={false}
         size="calc(100vw - 3rem)"
+        withCloseButton={false}
+        fullScreen={isMobile}
+        padding={0}
       >
-        <Post post="" />
+        <Post post={testPosts[0]!} />
       </Modal>
     </>
   );
