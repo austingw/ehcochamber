b8b9d53d292c153b3c01a6d1cb7b6c7424bc686d
austingw <senortinman@gmail.com>
2023-10-13 22:37:16 -0400

    Update signup function to create public profile

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -1,22 +1,25 @@
 import { z } from "zod";
-
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 
 export const authRouter = createTRPCRouter({
   signUp: publicProcedure
     .input(z.object({ email: z.string(), password: z.string() }))
     .mutation(async ({ input, ctx }) => {
-      await ctx.supabase.auth
-        .signUp({
+      const { data } = await ctx.supabase.auth.signUp({
+        email: input.email,
+        password: input.password,
+      });
+
+      await ctx.prisma.profile.create({
+        data: {
           email: input.email,
-          password: input.password,
-        })
-        .catch((error) => {
-          console.log("ERRRORRRRR", error);
-        })
-        .then((response) => {
-          console.log("HERE IT IS THE RESPONSE", response);
-          return response?.data;
-        });
+          name: input.email,
+          user: {
+            connect: {
+              id: data?.user?.id,
+            },
+          },
+        },
+      });
     }),
 });
